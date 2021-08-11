ab67f423c901043c14c16d2df56a914f881697bb
phonix007 <telegramtutorial007@gmail.com>
2021-08-11 13:41:27 +0530

    st

--- app/src/main/java/com/od/sharemarketmarathi/MainActivity.java
+++ app/src/main/java/com/od/sharemarketmarathi/MainActivity.java
@@ -1,14 +1,54 @@
 package com.od.sharemarketmarathi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import android.webkit.WebView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN); // hides status bar
+
+        navigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.body_cointainer,new HomeFragment()).commit();
+        navigationView.setSelectedItemId(R.id.nav_home);
+
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId()){
+
+                    case R.id.nav_home:
+                        fragment = new HomeFragment();
+                        break;
+
+                    case R.id.nav_book:
+                        fragment = new BookFragment();
+                        break;
+
+                    case R.id.nav_account:
+                        fragment = new AccountFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.body_cointainer,fragment).commit();
+
+                return true;
+            }
+        });
     }
 }
