4af0880d175e0a62eef6760e76ef6cd72a4e9709
phonix007 <telegramtutorial007@gmail.com>
2021-08-11 10:04:26 +0530

    st

---
+++ app/src/main/java/com/od/sharemarketmarathi/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.od.sharemarketmarathi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
