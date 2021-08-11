4fd8c1215a162f4e303144cdcf997277570a50f8
phonix007 <telegramtutorial007@gmail.com>
2021-08-11 09:57:58 +0530

    st

---
+++ app/src/main/java/com/od/sharemarketmarathi/MainActivity.java
@@ -0,0 +1,14 @@
+package com.od.sharemarketmarathi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
