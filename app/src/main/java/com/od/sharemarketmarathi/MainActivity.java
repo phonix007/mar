a0414a3e2bfadb1ee6877a3a51a027b151cec99c
phonix007 <telegramtutorial007@gmail.com>
2021-08-21 00:21:55 +0530

    st

--- app/src/main/java/com/od/sharemarketmarathi/MainActivity.java
+++ app/src/main/java/com/od/sharemarketmarathi/MainActivity.java
@@ -7,12 +7,19 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
 import android.webkit.WebView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
-public class MainActivity extends AppCompatActivity {
+import eu.dkaratzas.android.inapp.update.Constants;
+import eu.dkaratzas.android.inapp.update.InAppUpdateManager;
+import eu.dkaratzas.android.inapp.update.InAppUpdateStatus;
+
+public class MainActivity extends AppCompatActivity implements InAppUpdateManager.InAppUpdateHandler {
+    InAppUpdateManager inAppUpdateManager;
 
     BottomNavigationView navigationView;
 
@@ -22,8 +29,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN); // hides status bar
 
+        //update ke liye coding
+        inAppUpdateManager = InAppUpdateManager.Builder(this, 101)
+                .resumeUpdates(true)
+                .mode(Constants.UpdateMode.IMMEDIATE)   // Immdiate Means Forcefully Install And Felexiable means no thanks option
+                .snackBarAction("An update has just been downloaded")
+                .snackBarAction("RESTART")
+                .handler(this);
+        inAppUpdateManager.checkForAppUpdate();
+
         navigationView = findViewById(R.id.bottom_navigation);
-        getSupportFragmentManager().beginTransaction().replace(R.id.body_cointainer,new HomeFragment()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.body_cointainer, new HomeFragment()).commit();
         navigationView.setSelectedItemId(R.id.nav_home);
 
 
@@ -31,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Fragment fragment = null;
-                switch (item.getItemId()){
+                switch (item.getItemId()) {
 
                     case R.id.nav_home:
                         fragment = new HomeFragment();
@@ -49,10 +65,37 @@ public class MainActivity extends AppCompatActivity {
                         fragment = new AccountFragment();
                         break;
                 }
-                getSupportFragmentManager().beginTransaction().replace(R.id.body_cointainer,fragment).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.body_cointainer, fragment).commit();
 
                 return true;
             }
         });
+    }
+
+    @Override
+    public void onInAppUpdateError(int code, Throwable error) {
+
+    }
+
+    @Override
+    public void onInAppUpdateStatus(InAppUpdateStatus status) {
+
+        if (status.isDownloaded()) {
+            View view = getWindow().getDecorView().findViewById(android.R.id.content);
+            Snackbar snackbar = Snackbar.make(view,
+                    "App has been downloaded successfullly",
+                    Snackbar.LENGTH_INDEFINITE);
+            snackbar.setAction("", new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    inAppUpdateManager.completeUpdate();
+
+                }
+            });
+
+            snackbar.show();
+
+        }
+
     }
 }
